5a48cfb51b73e1c43f027bceb1da33683460ab5e
Muhamad Arsyad Ramadhan <marsyad.ramadhan@unsircle.com>
2022-05-23 09:53:35 +0700

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      rio
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
